0579353a50406babdee2765c01596c73851dd85c
Gabriela <gabrielacavalcanti6@gmail.com>
2026-05-11 23:04:46 -0300

    adds data-source file

---
+++ src/database/data-source.ts
@@ -0,0 +1,14 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true, //true - good for development, bad for production (use migration), change it later on
+  logging: false,
+  entities: [], //add entites later on
+});
