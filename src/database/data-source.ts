bc9208403ab214e78cfe0d364821e95d5b506b90
Gabriela <gabrielacavalcanti6@gmail.com>
2026-05-25 18:42:47 -0300

    adds .env.development file

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -3,12 +3,12 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  synchronize: true, //true - good for development, bad for production (use migration), change it later on
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: process.env.NODE_ENV === "development" ? true : false,
   logging: false,
   entities: [], //add entites later on
 });
