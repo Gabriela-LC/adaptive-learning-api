dc8d158104827219e40d52cf091c77c013b6a799
Gabriela <gabrielacavalcanti6@gmail.com>
2026-05-26 18:06:20 -0300

    adds DB initializer on server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
+import "dotenv/config";
 import "reflect-metadata";
 import app from "./app";
+import { AppDataSource } from "./database/data-source";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    console.log("📦 Database connected");
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("❌ Database connection error");
+    console.error(error);
+  });
